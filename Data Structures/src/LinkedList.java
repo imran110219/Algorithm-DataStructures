26e643e7c7313a0159012e375ba096f0f5f1cf05
imran110219 <imran110219@gmail.com>
2019-12-27 21:10:24 +0600

    Linked List

--- Data Structures/src/LinkedList.java
+++ Data Structures/src/LinkedList.java
@@ -19,18 +19,100 @@ public class LinkedList {
         return (node==null);
     }
 
-    public void insert(int newData){
+    public void insertFirst(int newData){
         Node node = new Node(newData);
         node.next = head;
         head = node;
     }
 
+    public void insertByPosition(int newData, int position){
+        Node node = new Node(newData);
+        int counter = 1;
+        Node temp = head;
+        while (counter<position-1){
+            temp = temp.next;
+            counter++;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+
     public void insertLast(int newData){
         Node node = new Node(newData);
-//        while (){
-//
-//        }
-        node.next = head;
-        head = node;
+        if(head == null)
+            head = node;
+        else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void deleteByKey(int newData){
+        int flag = 0;
+        Node temp = head;
+        if(temp != null && temp.data == newData){
+            head = temp.next;
+            flag = 1;
+        }
+        else {
+            while (temp.next != null) {
+                if (temp.next.data == newData) {
+                    temp.next = temp.next.next;
+                    flag = 1;
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+        if(flag == 0)
+            System.out.println(newData+ " not found.");
+    }
+
+    public void deleteByPosition(int position){
+        int counter = 1;
+        int flag = 0;
+        Node temp = head;
+        if(temp != null && position == 1){
+            head = temp.next;
+            flag = 1;
+        }
+        else {
+            while (counter < position - 1) {
+                temp = temp.next;
+                counter++;
+            }
+            flag = 1;
+            temp.next = temp.next.next;
+        }
+        if(flag == 0)
+            System.out.println(position+ " not found.");
+    }
+
+    public void printList(){
+        while(head != null){
+            System.out.print(head.data+ " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insertLast(1);
+        list.insertLast(3);
+        list.insertLast(4);
+        list.insertLast(5);
+        list.insertByPosition(2,2);
+        list.insertFirst(0);
+
+        list.deleteByKey(0);
+        list.deleteByPosition(2);
+        list.deleteByKey(-1);
+//        System.out.println(list.head.next.data);
+//        System.out.println(list.isEmpty(list.head));
+
+        list.printList();
     }
 }
