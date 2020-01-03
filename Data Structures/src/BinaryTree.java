7265b0673977c491154c6743cbc6f5d84742e105
imran110219 <imran110219@gmail.com>
2020-01-04 01:08:27 +0600

    Binary Tree Initialize

---
+++ Data Structures/src/BinaryTree.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Imran on 1/4/2020.
+ */
+public class BinaryTree {
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            right = null;
+            left = null;
+        }
+    }
+}
