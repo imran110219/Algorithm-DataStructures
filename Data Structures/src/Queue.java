4624f0385fe1bfd693d8f3ffcf8a00197b7fa4ac
imran110219 <imran110219@gmail.com>
2020-01-03 21:13:27 +0600

    Queue Implement

--- Data Structures/src/Queue.java
+++ Data Structures/src/Queue.java
@@ -9,7 +9,8 @@ public class Queue {
     public Queue(int capacity) {
         this.capacity = capacity;
         arr = new int[capacity];
-        front = size;
+        front = 0;
+        rear = -1;
     }
 
     public boolean isFull(){
@@ -20,11 +21,50 @@ public class Queue {
         return size == 0;
     }
 
+    public int peek(int data){
+        return arr[front];
+    }
+
     public void enqueue(int data){
         if(isFull())
             System.out.println("Queue Overflow");
         else{
-
+            rear++;
+            if(rear == capacity-1)
+                rear = 0;
+            arr[rear] = data;
+            size++;
         }
+    }
+
+    public void dequeue(){
+        if(isEmpty())
+            System.out.println("Queue Underflow");
+        else{
+            front++;;
+            if(front == capacity - 1)
+                front = 0;
+            size--;
+        }
+    }
+
+    public void printQueue(){
+        int start = front;
+        int end = rear;
+        for(int i =0; i<capacity; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.printQueue();
+//        queue.enqueue(6);
     }
 }
