af3e555cec08d4513ac8867637df1d97bfacc53b
imran110219 <imran110219@gmail.com>
2020-01-05 21:30:45 +0600

    Sorting Algorithm Initialize

---
+++ Algorithms/src/BubbleSort.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Imran on 1/5/2020.
+ */
+public class BubbleSort {
+    public void bubbleSort(int arr[]){
+        int len = arr.length;
+        for(int i=0; i<len-1; i++)
+            for(int j=0; j<len-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+    }
+
+    public void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        int array []= {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubbleSort.bubbleSort(array);
+        bubbleSort.printArray(array);
+    }
+}
