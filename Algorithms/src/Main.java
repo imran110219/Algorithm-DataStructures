1358f9e33a56a4f92ecd104e17c9deebc2961194
imran110219 <imran110219@gmail.com>
2020-01-26 22:30:59 +0600

    Time and Memory

--- Algorithms/src/Main.java
+++ Algorithms/src/Main.java
@@ -1,6 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TimeTest1 {
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
+        long total = 0;
+        for (int i = 0; i < 10000000; i++) {
+            total += i;
+        }
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println(elapsedTime);
+    }
+}
+
 public class Main {
+    private static final long MEGABYTE = 1024L * 1024L;
+
+    public static long bytesToMegabytes(long bytes) {
+        return bytes / MEGABYTE;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i <= 100000; i++) {
+            list.add(new String("Jim"));
+        }
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is bytes: " + memory);
+        System.out.println("Used memory is megabytes: "
+                + bytesToMegabytes(memory));
     }
 }
