dd7a165350731fd8efcb463a550b33eafcb6d0e3
imran110219 <imran110219@gmail.com>
2020-02-14 11:48:48 +0600

    knapsack algorithm initialize

---
+++ Algorithms/src/main/java/greedymethod/Knapsack.java
@@ -0,0 +1,45 @@
+package greedymethod;
+
+/**
+ * Created by Imran on 2/14/2020.
+ */
+public class Knapsack {
+    private int numOfItems;
+    private int capacityOfKnapsack;
+    private int[][] knapsackTable;
+    private int totalBenefit;
+    private int[] weights;
+    private int[] values;
+
+    public Knapsack(int numOfItems, int capacityOfKnapsack, int[] weights, int[] values) {
+        this.numOfItems = numOfItems;
+        this.capacityOfKnapsack = capacityOfKnapsack;
+        this.weights = weights;
+        this.values = values;
+    }
+
+    public void solve() {
+        // time complexity: O(N*W)
+        for(int i=1; i<=numOfItems; i++){
+            for(int w=1; w<=capacityOfKnapsack; w++){
+                int notTakingItem = knapsackTable[i-1][w];
+                int takingItem = 0;
+                if(weights[i] <= w) {
+                    takingItem = knapsackTable[i-1][w-weights[i]];
+                }
+                knapsackTable[i][w] = Math.max(notTakingItem, takingItem);
+            }
+        }
+        totalBenefit = knapsackTable[numOfItems][capacityOfKnapsack];
+        System.out.println(totalBenefit);
+    }
+
+    public static void main(String[] args) {
+        int numOfItems = 4;
+        int[] weights = {10, 40, 20, 30};
+        int[] values = {60, 40, 100, 120};
+        int capacityOfKnapsack = 50;
+        Knapsack knapsack = new Knapsack(numOfItems, capacityOfKnapsack, weights, values);
+        knapsack.solve();
+    }
+}
