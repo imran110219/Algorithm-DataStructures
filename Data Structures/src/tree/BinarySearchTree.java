9246c236bb4007fa7082e03a32f030bdeae8551a
Sadman <imran110219@gmail.com>
2020-03-10 16:26:15 +0600

    tree reform

--- Data Structures/src/tree/BinarySearchTree.java
+++ Data Structures/src/tree/BinarySearchTree.java
@@ -7,6 +7,8 @@ import java.io.*;
 
 /**
  * Created by Imran on 1/6/2020.
+ * input : 7
+ * 4 2 6 1 3 5 7
  */
 
 public class BinarySearchTree {
@@ -24,11 +26,11 @@ public class BinarySearchTree {
     }
 
     public static Node insert(Node root, int data) {
-        if(root == null) {
+        if (root == null) {
             return new Node(data);
         } else {
             Node cur;
-            if(data <= root.data) {
+            if (data <= root.data) {
                 cur = insert(root.left, data);
                 root.left = cur;
             } else {
@@ -40,42 +42,42 @@ public class BinarySearchTree {
     }
 
     public static void preOrder(Node root) {
-        if(root == null)
+        if (root == null)
             return;
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
     public static void inOrder(Node root) {
-        if(root == null)
+        if (root == null)
             return;
         inOrder(root.left);
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         inOrder(root.right);
     }
 
     public static void postOrder(Node root) {
-        if(root == null)
+        if (root == null)
             return;
         postOrder(root.left);
         postOrder(root.right);
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
     }
 
     public static void levelOrder(Node root) {
         Queue<Node> queue = new LinkedList<>();
-        if(root == null)
+        if (root == null)
             return;
         else {
             queue.add(root);
-            while(!queue.isEmpty()){
+            while (!queue.isEmpty()) {
                 Node temp = queue.remove();
-                System.out.print(temp.data+" ");
-                if(temp.left != null){
+                System.out.print(temp.data + " ");
+                if (temp.left != null) {
                     queue.add(temp.left);
                 }
-                if(temp.right != null){
+                if (temp.right != null) {
                     queue.add(temp.right);
                 }
             }
@@ -83,37 +85,52 @@ public class BinarySearchTree {
     }
 
     public static Node lca(Node root, int v1, int v2) {
-        if(v1 > root.data && v2 > root.data)
+        if (v1 > root.data && v2 > root.data)
             return lca(root.right, v1, v2);
-        if(v1 < root.data && v2 < root.data)
+        if (v1 < root.data && v2 < root.data)
             return lca(root.left, v1, v2);
         return root;
     }
 
     public static int height(Node root) {
-        if(root == null)
+        if (root == null)
             return -1;
-        return Math.max(height(root.left),height(root.right))+1;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    public static int countLeaves(Node root) {
+        if (root == null)
+            return 0;
+        else if (root.left == null && root.right == null)
+            return 1;
+        else
+            return countLeaves(root.left) + countLeaves(root.right);
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         Node root = null;
-        while(t-- > 0) {
+        while (t-- > 0) {
             int data = scan.nextInt();
             root = insert(root, data);
         }
         scan.close();
+        System.out.print("In Order : ");
         inOrder(root);
         System.out.println();
+        System.out.print("Pre Order : ");
         preOrder(root);
         System.out.println();
+        System.out.print("Post Order : ");
         postOrder(root);
         System.out.println();
+        System.out.print("Level Order : ");
         levelOrder(root);
         System.out.println();
+        System.out.print("Height : ");
         System.out.println(height(root));
-
+        System.out.print("Number of Leaves : ");
+        System.out.println(countLeaves(root));
     }
 }
