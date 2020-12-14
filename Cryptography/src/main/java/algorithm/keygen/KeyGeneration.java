9028382e34a3e67a328d8d0606f5951b3031c363
Sadman Sobhan <imran110219@gmail.com>
2020-12-14 21:45:11 +0600

    Cryptography

--- Cryptography/src/main/java/algorithm/keygen/KeyGeneration.java
+++ Cryptography/src/main/java/algorithm/keygen/KeyGeneration.java
@@ -1,10 +1,14 @@
 package algorithm.keygen;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
 
 import java.security.Key;
 import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 /**
@@ -13,18 +17,33 @@ import java.util.List;
 public class KeyGeneration {
     public static void main(String args[]) throws Exception{
 
-        List<String> algorithms = Arrays.asList("AES", "DES", "DESede", "HmacSHA1", "HmacSHA256");
+        List<String> algorithms = Arrays.asList("DES", "AES", "DESede", "HmacSHA256", "HmacSHA1");
         //AES (128)
         //DES (56)
         //DESede (168)
         //HmacSHA1
         //HmacSHA256
 
+        List<Key> keys = new ArrayList<>();
+
         SecureRandom secRandom = new SecureRandom();
         for (String algorithm : algorithms) {
             KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
             Key key = keyGen.generateKey();
-            System.out.println(key);
+            keys.add(key);
+            System.out.println(key.getEncoded().length + "  " + key.getAlgorithm() + "  " + convertKeyToString(key));
+            System.out.println(convertStringToKey(convertKeyToString(key),key.getAlgorithm()));
+
         }
+    }
+
+    public static String convertKeyToString(Key key){
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static Key convertStringToKey(String keyString, String algorithm){
+        byte[] decodedKey = Base64.getDecoder().decode(keyString);
+        Key key = new SecretKeySpec(decodedKey, 0, decodedKey.length, algorithm);
+        return key;
     }
 }
