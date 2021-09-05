97770cd0d3433bdf031748ed9238b2aa12b34e97
Sadman <imran110219@gmail.com>
2021-09-05 16:24:33 +0600

    test file deleted

--- Algorithms/src/main/java/searching/DepthFirstSearchList.java
+++ Algorithms/src/main/java/searching/DepthFirstSearchList.java
@@ -8,77 +8,70 @@ import java.util.Stack;
  * Created by Imran on 2/5/2020.
  */
 public class DepthFirstSearchList {
-    static class Node
-    {
+    static class Node {
         int data;
         boolean visited;
         List<Node> neighbours;
 
-        Node(int data)
-        {
-            this.data=data;
-            this.neighbours=new ArrayList<>();
+        Node(int data) {
+            this.data = data;
+            this.neighbours = new ArrayList<>();
 
         }
-        public void addneighbours(Node neighbourNode)
-        {
+
+        public void addneighbours(Node neighbourNode) {
             this.neighbours.add(neighbourNode);
         }
+
         public List<Node> getNeighbours() {
             return neighbours;
         }
+
         public void setNeighbours(List<Node> neighbours) {
             this.neighbours = neighbours;
         }
     }
 
-    public  void dfs(Node node)
-    {
+    public void dfs(Node node) {
         System.out.print(node.data + " ");
-        List<Node> neighbours=node.getNeighbours();
-        node.visited=true;
+        List<Node> neighbours = node.getNeighbours();
+        node.visited = true;
         for (int i = 0; i < neighbours.size(); i++) {
-            Node n=neighbours.get(i);
-            if(n!=null && !n.visited)
-            {
+            Node n = neighbours.get(i);
+            if (n != null && !n.visited) {
                 dfs(n);
             }
         }
     }
 
-    public  void dfsUsingStack(Node node)
-    {
-        Stack<Node> stack=new  Stack<Node>();
+    public void dfsUsingStack(Node node) {
+        Stack<Node> stack = new Stack<Node>();
         stack.add(node);
-        while (!stack.isEmpty())
-        {
-            Node element=stack.pop();
-            if(!element.visited)
-            {
+        while (!stack.isEmpty()) {
+            Node element = stack.pop();
+            if (!element.visited) {
                 System.out.print(element.data + " ");
-                element.visited=true;
+                element.visited = true;
             }
 
-            List<Node> neighbours=element.getNeighbours();
+            List<Node> neighbours = element.getNeighbours();
             for (int i = 0; i < neighbours.size(); i++) {
-                Node n=neighbours.get(i);
-                if(n!=null && !n.visited)
-                {
+                Node n = neighbours.get(i);
+                if (n != null && !n.visited) {
                     stack.add(n);
                 }
             }
         }
     }
 
-    public static void main(String arg[])
-    {
-        Node node40 =new Node(40);
-        Node node10 =new Node(10);
-        Node node20 =new Node(20);
-        Node node30 =new Node(30);
-        Node node60 =new Node(60);
-        Node node50 =new Node(50);
-        Node node70 =new Node(70);
+    public static void main(String arg[]) {
+        Node node40 = new Node(40);
+        Node node10 = new Node(10);
+        Node node20 = new Node(20);
+        Node node30 = new Node(30);
+        Node node60 = new Node(60);
+        Node node50 = new Node(50);
+        Node node70 = new Node(70);
 
         node40.addneighbours(node10);
         node40.addneighbours(node20);
@@ -99,13 +92,13 @@ public class DepthFirstSearchList {
         System.out.println();
 
         // Resetting the visited flag for nodes
-        node40.visited=false;
-        node10.visited=false;
-        node20.visited=false;
-        node30.visited=false;
-        node60.visited=false;
-        node50.visited=false;
-        node70.visited=false;
+        node40.visited = false;
+        node10.visited = false;
+        node20.visited = false;
+        node30.visited = false;
+        node60.visited = false;
+        node50.visited = false;
+        node70.visited = false;
 
 
         System.out.println("The DFS traversal of the graph using recursion ");
